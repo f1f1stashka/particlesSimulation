f61ccc12e7b82fa0e7691ab035263a2e7f3ec05a
changeThis! <changeThis!>
2023-01-12 19:35:14 +0300

    feat: слишком много изменений я психанул.

--- Main.java
+++ Main.java
@@ -4,27 +4,41 @@ import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import static java.lang.Math.*;
+import static java.util.Random.*;
 
 public class Main {
     public static int _size = 800;
-    static Particle[] particles = new Particle[30];
+    static Particle[] particles = new Particle[15];
+
+    public static void goAllParticles(){
+        for(int i = 0; i < particles.length; i++){
+            particles[i].goTo();
+        }
+    }
 
     public static void handleParticlesForces(){
         for (int i1 = 0; i1 < particles.length; i1++){
-            Vector[] vectors = new Vector[particles.length];
-            Vector averageVector = new Vector(0, 0);
-            particles[i1].vector.value = 0;
-            particles[i1].vector.angle = 0;
             for(int i2 = 0; i2 < particles.length; i2++){
-                vectors[i2] = particles[i2].vector;
+                if(i2 == i1){
+                    continue;
+                }
+                if(isInRadius(particles[i1], particles[i2], 30)){
+
+                    //particles[i1].vector = Vector.getAverageVector(particles[i2].vector, particles[i1].vector);
+                    if(particles[i1].charge == -1 && particles[i2].charge == 1) {
+                        particles[i1].vector.angle = getAngleFromDots(particles[i2].x - particles[i1].x, particles[i2].y - particles[i1].y)-270;
+                        particles[i1].vector.value = 0.5;
+                        particles[i1].goTo();
+                        particles[i1].vector.value = 1;
+                    } else if(particles[i2].charge == -1 && particles[i1].charge == 1){
+                        particles[i2].vector.angle = getAngleFromDots(particles[i1].x - particles[i2].x, particles[i1].y - particles[i2].y)-270;
+                        particles[i2].vector.value = 0.5;
+                        particles[i2].goTo();
+                        particles[i2].vector.value = 1;
+                    }
+                }
             }
-            for(int j = 0; j < vectors.length; j++){
-                averageVector.value += vectors[j].value;
-                averageVector.angle += vectors[j].angle;
-            }
-            averageVector.value = averageVector.value/vectors.length;
-            averageVector.angle = averageVector.angle/vectors.length;
-            particles[i1].vector = averageVector;
+
         }
     }
 
@@ -86,10 +100,10 @@ public class Main {
 
     public static void repulseParticlesIfCollisioning(Particle p1, Particle p2) {
         if(p1.x == p2.x){
-            p2.goTo(getRandCharge(), getRandCharge());
+            p2.goTo();
         }
         if(p1.y == p2.y){
-            p2.goTo(getRandCharge(), getRandCharge());
+            p2.goTo();
         }
     }
 
@@ -109,7 +123,7 @@ public class Main {
             if (particles[j].charge == 0) {
                 imgG.setColor(new Color(0xffffff));
             }
-            //imgG.drawOval(particles[j].x-30, particles[j].y-30, 60, 60);
+            imgG.drawOval(particles[j].x-30, particles[j].y-30, 60, 60);
             imgG.fillRect(particles[j].x-1, particles[j].y-1, 2, 2);
         }
 
@@ -130,12 +144,22 @@ public class Main {
         return -1;
     }
 
-    public static int getAngleFromTwoDots(int x1, int y1, int x2, int y2) {
-        return (int) atan2(y2-y1, x2-x1);
+    public static double getAngleFromDots(int x, int y){
+        try{
+            return (double) (acos(y/sqrt(x*x+y*y))*y/abs(y)*180)/PI;
+        }catch (ArithmeticException e){
+            return 90;
+        }
+
     }
+
+    //public static int getLengthFromDots(int x1, int y1, int x2, int y2){
+
+    //}
 
 
     public static void main(String[] args) throws InterruptedException {
+
         JFrame frame = new JFrame();
         Canvas canvas = new Canvas();
         frame.setSize(_size+16, _size+38);
@@ -143,11 +167,10 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         for(int i = 0; i < particles.length; i++){
-            particles[i] = new Particle(400, 400, getRandRange(-1, 2), 1, new Vector(0, 0));
+            particles[i] = new Particle(getRandRange(100, 700), getRandRange(100, 700), getRandCharge(), 1, new Vector(getRandRange(0, 360), 5));
         }
-
         while(true){
-            //Thread.sleep(100);
+            Thread.sleep(100);
             renderMap(frame);
             handleParticlesForces();
         }
@@ -170,36 +193,35 @@ class Particle{
         this.charge = charge;
         this.mass = mass;
         this.vector = vector;
-        System.out.println(charge);
+        System.out.println(vector.angle);
     }
 
     void goTo(){
+        if(vector.angle > 360){
+            vector.angle -= 360;
+        }else if(vector.angle < 0){
+            vector.angle += 360;
+        }else if(Double.isNaN(vector.angle)){
+            vector.angle = 0;
+        }
         x += sin(toRadians(this.vector.angle))*this.vector.value;
         y += cos(toRadians(this.vector.angle))*this.vector.value;
     }
 }
 
 class Vector{
-    int angle;
-    int value;
+    double angle;
 
-    Vector(int angle, int value){
+    double value;
+
+    Vector(double angle, double value){
         this.angle = angle;
         this.value = value;
     }
 
-    // TODO: \/
-    // public static Vector calculateAvgVectorFromArray(Vector[] vectors) {}
-
-    public static Vector calculateAvgVector(Vector vec1, Vector vec2) {
-        float angleDif = abs(vec1.angle - vec2.angle);
-        int resultVectorValue = (int) (
-                pow(vec1.value, 2) + pow(vec2.value, 2) 
-                - 2 * vec1.value * vec2.value * cos(180 - angleDif)
-            ) / 2;
-        int resultVectorAngle = 
-            (int) asin(vec2.value * sin(180 - angleDif) / resultVectorValue);
-
-        return new Vector(resultVectorAngle, resultVectorValue);
+    public static Vector getAverageVector(Vector vector1, Vector vector2){
+        double jAngle = (int) (pow(abs(vector1.angle), 2) + pow(abs(vector2.angle), 2) - 2 * abs(vector1.angle) * abs(vector2.angle) * cos(toRadians(180-(vector2.angle-vector1.angle))));
+        double jValue = vector2.value + vector1.value;
+        return new Vector(jAngle, jValue);
     }
 }
